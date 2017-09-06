e1a5c2ec7b9c8f357308e7557f099d32f7ee14cc
avadarad <avadanii@tiscali.it>
2017-09-06 12:42:26 +0300

    organized the gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,2 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch');
-
-gulp.task('default', function() {
-  console.log('Hooay!');
-});
-
-gulp.task('html', function() {
-  console.log('html');
-});
-
-gulp.task('watch', function() {
-  watch('./app/index.html', function() {
-    gulp.start('html');
-  });
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
